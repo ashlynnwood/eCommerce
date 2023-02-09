70ba7be1215fda02b1d980dc32553ccf7aeaf1ed
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-08 21:39:36 -0600

    Initial code

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+
+// product one2one with category
+// product = parent, cat = child
+
+// product has many2many with tags
+// one product belongs to multiple tags
+// and mult tags can belong to mult products
+
+// steps:
+// 1. make sure you can connect to sequelzie
+// 2. correct models
+// 3. correct routes
+// 4. join tables together
+
+// each cat has mult products
+// assign cat_id to product
+
+// each product has mult tag
+
+// tag
+
+// product belongs to category
+
