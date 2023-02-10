eb97175d6a505cbf833161b4862181a75245835d
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-09 22:04:24 -0600

    Product get and delete routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,8 +110,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+  
+    if (!products) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 module.exports = router;
